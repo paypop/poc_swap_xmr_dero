9b204ff313918e4ca675f80dd10b8e2cf4fe451b
netwarp <resethread@gmail.com>
2021-05-15 16:52:27 +0200

    add WalletService class

---
+++ src/WalletService.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Swap;
+
+use GuzzleHttp\Client;
+
+
+class WalletService
+{
+    /**
+     * @var string
+     */
+    private string $rpc_address;
+
+    /**
+     * @var string
+     */
+    private string $currency;
+
+    /**
+     * WalletService constructor.
+     * @param string $rpc_address
+     * @param string $currency
+     */
+    function __construct(string $rpc_address, string $currency = 'dero')
+    {
+        $this->rpc_address = $rpc_address;
+        $this->currency = $currency;
+    }
+
+    /**
+     * @param string $method
+     * @param null $params
+     * @param string $currency
+     */
+    private function request(string $method, $params = null)
+    {
+        $data = [
+            'id' => '1',
+            'method' => $method
+        ];
+
+        if ($this->currency === 'dero') {
+            $data['jsonrpc'] = '2.0';
+        }
+        else {
+            $data['json_rpc'] = '2.0';
+        }
+
+        if ($params) {
+            $data['params'] = $params;
+        }
+
+        $client = new Client([
+            'base_uri' => $this->rpc_address,
+
+            'timeout' => '2.0'
+        ]);
+
+        $response = $client->request('POST', 'json_rpc', [
+            'json' => $data
+        ]);
+        $body = $response->getBody()->getContents();
+
+        $body = json_decode($body);
+
+        $result = $body->result;
+
+        return $result;
+    }
+
+    public function getAddress()
+    {
+        $method = '';
+
+        if ($this->currency == 'dero') {
+            $method = 'getaddress';
+        }
+        else {
+            $method = 'get_address';
+        }
+
+        return $this->request($method);
+    }
+
+    public function makeIntegratedAddress()
+    {
+        return $this->request('make_integrated_address');
+    }
+
+
+}
