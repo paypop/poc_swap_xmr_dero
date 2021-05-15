8ffa0c59dbeab60e030ef01923141c1e9b0e6312
netwarp <resethread@gmail.com>
2021-05-15 16:53:21 +0200

    add function color

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -46,3 +46,23 @@ function convertSwapSatToSat(float $value_btc_coin_a, float $value_btc_coin_b, i
 
     return $coin_b_atomic;
 }
+
+function colorLog(string $str, string $type = '') {
+    switch ($type) {
+        case 'e': //error
+            echo "\033[31m$str \033[0m\n";
+            break;
+        case 's': //success
+            echo "\033[32m$str \033[0m\n";
+            break;
+        case 'w': //warning
+            echo "\033[33m$str \033[0m\n";
+            break;
+        case 'i': //info
+            echo "\033[36m$str \033[0m\n";
+            break;
+        default:
+            echo "$str \n";
+            break;
+    }
+}
