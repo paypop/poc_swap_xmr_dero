0cca180fc9209cf6d9c637fef21815b09484fee3
netwarp <resethread@gmail.com>
2021-05-22 10:13:55 +0200

    move a assert to another function

--- tests/MakeSomeTest.php
+++ tests/MakeSomeTest.php
@@ -8,9 +8,6 @@ final class MakeSomeTest extends TestCase
 {
     public function test1() {
         $this->assertEquals(1e5, 100_000);
-    }
-
-    public function test2() {
         $this->assertEquals(1e12, 1_000_000_000_000);
     }
 
@@ -18,5 +15,4 @@ final class MakeSomeTest extends TestCase
         $result = convertSwapSatToSat(0.00011302, 0.00678127, 10000, 1000000000000, 40_000);
         dd($result);
     }
-
 }
