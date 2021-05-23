c49188c2918f2a5261cf7b223915225ecacd5605
netwarp <resethread@gmail.com>
2021-05-23 13:50:13 +0200

    rename to ConverterTest

--- tests/ConvertTest.php
+++ tests/ConvertTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class MakeSomeTest extends TestCase
+final class ConvertTest extends TestCase
 {
     public function test1() {
         $this->assertEquals(1e5, 100_000);
@@ -12,7 +12,7 @@ final class MakeSomeTest extends TestCase
     }
 
     public function test3() {
-        $result = convertSwapSatToSat(0.00011302, 0.00678127, 10000, 1000000000000, 40_000);
+        $result = convertSwapSatToSat(100_000, 0.00678127, 10000, 1000000000000, 40_000);
         dd($result);
     }
 }
